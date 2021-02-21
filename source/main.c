025cafce0a64d81f9803121f2fa93dc32dc06d41
36tofu <christopher99chen@gmail.com>
2021-02-20 22:39:40 -0800

    backing up sat night work

--- source/main.c
+++ source/main.c
@@ -1,17 +1,112 @@
+
 /*	Author: lab
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: PORTB = tmpBT1;Lab #  Exercise #
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
- *	CHECK VIDEO DEMO REQUIREMENTS 
- */ 
+ */
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
+
+unsigned long _avr_timer_M = 1; //start count from here, down to 0. Dft 1ms
+unsigned long _avr_timer_cntcurr = 0; //Current internal count of 1ms ticks
+
+void TimerOn(){
+	//AVR timer/counter controller register TCCR1
+	TCCR1B = 0x0B; //bit 3 = 0: CTC mode (clear timer on compare)
+	//AVR output compare register OCR1A
+	OCR1A = 125; // Timer interrupt will be generated when TCNT1 == OCR1A
+	//AVR timer interrupt mask register
+	TIMSK1 = 0x02; //bit1: OCIE1A -- enables compare match interrupt
+	//Init avr counter
+	TCNT1 = 0;
+
+	_avr_timer_cntcurr = _avr_timer_M;
+	//TimerISR will be called every _avr_timer_cntcurr ms
+	
+	//Enable global interrupts 
+	SREG |= 0x80; //0x80: 1000000
+
+}
+
+void TimerOff(){
+	TCCR1B = 0x00; //bit3bit1bit0 = 000: timer off
+}
+
+
+ISR(TIMER1_COMPA_vect){
+	_avr_timer_cntcurr--;
+	if (_avr_timer_cntcurr == 0) {
+			TimerISR();
+			_avr_timer_cntcurr = _avr_timer_M;
+			}
+}
+
+void TimerSet(unsigned long M) {
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+
+
+
+
+typedef struct task {
+  int state; // Current state of the task
+  unsigned long period; // Rate at which the task should tick
+  unsigned long elapsedTime; // Time since task's previous tick
+  int (*TickFct)(int); // Function to call for task's tick
+} task;
+
+task tasks[3];
+
+const unsigned char tasksNum = 3;
+
+
+const unsigned long tasksPeriodGCD = 1;
+const unsigned long periodKP = 100;
+
+/*
+const unsigned long periodThreeLEDs = 300;
+unsigned long periodSpeaker = 2;
+const unsigned long periodCombined = 1;
+const unsigned long periodSample = 200;
+*/
+
+enum KP_States { KP_SMStart, KP_wait, KP_pressed };
+int TickFct_KP(int state);
+
+enum SQ_States { SQ_SMStart, SQ_init, SQ_begin, SQ_wait, SQ_check, SQ_match };
+int TickFct_detectSQ(int state);
+
+/*
+enum combined_States { C_SMStart, C_s1 };
+int TickFct_Combined(int state);
+
+enum SP_States { SP_SMStart, SP_s0, SP_s1};
+int TickFct_Speaker(int state);
+
+enum FRQ_States { FRQ_SMStart, FRQ_s0, FRQ_inc, FRQ_dec};
+int TickFct_FRQ(int state);
+
+*/
+
+void TimerISR() {
+  unsigned char i;
+  for (i = 0; i < tasksNum; ++i) { // Heart of the scheduler code
+     if ( tasks[i].elapsedTime >= tasks[i].period ) { // Ready
+        tasks[i].state = tasks[i].TickFct(tasks[i].state);
+        tasks[i].elapsedTime = 0;
+     }
+     tasks[i].elapsedTime += tasksPeriodGCD;
+  }
+}
 
 
 /*
@@ -55,6 +150,316 @@ unsigned char GetKeypadKey(){
 	return('\0');
 }
 
+//Queue Functions 
+typedef struct _Q7uc {
+   unsigned char buf[7];
+   unsigned char cnt;
+} Q7uc;
+
+void Q7ucInit(Q7uc *Q) {
+   (*Q).cnt=0;
+}
+
+unsigned char Q7ucFull(Q7uc Q) {
+   return (Q.cnt == 7);
+}
+
+unsigned char Q7ucEmpty(Q7uc Q) {
+   return (Q.cnt == 0);
+}
+
+void Q7ucPrint(Q7uc Q) {
+   int j;
+   printf("Q7uc contents: \r\n");
+   for (j=0; j<7; j++) {
+           printf("Item %d", j);
+           printf(": %d\r\n", Q.buf[j]);
+   }
+}
+
+void Q7ucPush(Q7uc *Q,
+                  unsigned char item) {
+   if (!Q7ucFull(*Q)) {
+      DisableInterrupts();
+      (*Q).buf[(*Q).cnt] = item;
+      (*Q).cnt++;
+      EnableInterrupts();
+   }
+}
+
+unsigned char Q7ucPop(Q7uc *Q)
+{
+   int i;
+   unsigned char item=0;
+   if (!Q7ucEmpty(*Q)) {
+      DisableInterrupts();
+      item = (*Q).buf[0];
+      (*Q).cnt--;
+      for (i=0; i<(*Q).cnt; i++) {
+         // shift fwd
+         (*Q).buf[i]=
+         (*Q).buf[i+1];
+      }
+      EnableInterrupts();
+   }
+   return(item);
+}
+
+unsigned char tmpDT1;
+unsigned char tmpDT2;
+unsigned char tmpBT1;
+unsigned char keyPressed;
+Q7uc btnQ;
+
+int main() {
+
+  DDRC = 0xF0; PORTC = 0x0F;
+  DDRB = 0x80; PORTB = 0x00;
+  unsigned char i=0;
+  tasks[i].state = KP_SMStart;
+  tasks[i].period = periodKP;
+  tasks[i].elapsedTime = tasks[i].period;
+  tasks[i].TickFct = &TickFct_KP;
+  
+  TimerOn();
+
+  
+  
+  while(1) {
+     PORTB = tmpBT1;
+  }
+  return 0;
+}
+
+int TickFct_KP(int state) {
+  switch(state) { // Transitions
+     case KP_SMStart: // Initial transition
+        state = KP_wait;
+        break;
+     case KP_wait:
+	keyPressed = GetKeypadKey();
+	if(keyPressed == '\0')
+		state = KP_wait;
+	else{ 
+		state = KP_pressed;
+		if(!Q7ucFULL(btnQ)){
+			Q7ucPush(&btnQ, keyPressed);
+		}
+	}
+        break;
+     case KP_pressed:
+	keyPressed = GetKeypadKey();
+	if(keyPressed == '\0')
+		state = KP_wait;
+	else 
+		state = KP_pressed;
+        break;
+     default:
+        state = KP_SMStart;
+   } // Transitions
+  switch(state) { // State actions
+     case KP_wait:
+        break;
+     case KP_pressed:
+        tmpBT1 = 0x80;
+	break;
+     default:
+        break;
+  } // State actions
+  //PORTD = tmpDT1;
+  return state;
+}
+
+int TickFct_detectSQ(int state) {
+  unsigned char recBtn;
+  const unsigned char seq[7] = {1,2,3,4,5};
+  static unsigned char i = 0;
+  switch(state) { // Transitions
+     case SQ_SMStart: // Initial transition
+        state = SQ_init;
+        break;
+     case SQ_init:
+        state = SQ_begin;
+        break;
+     case SQ_begin:
+        state = SQ_wait;
+        break;
+     case SQ_wait:
+	if(Q7ucEmpty(btnQ))state = SQ_wait;
+	else{
+		recBtn = Q7ucPop(&btnQ);
+		state = SQ_Check;
+	}
+        break;
+     case SQ_check:
+	if(recBtn == seq[i])state = SQ_match;
+	else state = SQ_begin;
+        break;
+     case SQ_match:
+	if(i!=5){
+		i++;
+		state = SQ_wait;
+	}
+	else state = SQ_begin;
+        break;
+     default:
+        state = SQ_SMStart;
+   } // Transitions
+//WE WERE HERE SATURDAY NIGHT
+//FINISH STATE ACTIONS 
+//INIT QUEUE 
+  switch(state) { // state actions
+     case SQ_SMStart: // 
+        break;
+     case SQ_init:
+	i = 0;
+        break;
+     case SQ_begin:
+        state = SQ_wait;
+        break;
+     case SQ_wait:
+	if(Q7ucEmpty(btnQ))state = SQ_wait;
+	else{
+		recBtn = Q7ucPop(&btnQ);
+		state = SQ_Check;
+	}
+        break;
+     case SQ_check:
+	if(recBtn == seq[i])state = SQ_match;
+	else state = SQ_begin;
+        break;
+     case SQ_match:
+	if(i!=5){
+		i++;
+		state = SQ_wait;
+	}
+	else state = SQ_begin;
+        break;
+     default:
+        state = SQ_SMStart;
+   } // Transitions
+  switch(state) { // State actions
+     case SQ_s1:
+	tmpDT2 = 0x01;
+        break;
+     case SQ_s2:
+	tmpDT2 = 0x02;
+        break;
+     case SQ_s3:
+	tmpDT2 = 0x04;
+        break;
+     default:
+        break;
+  } // State actions
+  //PORTD = tmpDT2;
+  return state;
+}
+
+/*
+int TickFct_Combined(int state) {
+  switch(state) { // Transitions
+     case C_SMStart: // Initial transition
+        state = C_s1;
+        break;
+     case C_s1:
+        state = C_s1;
+        break;
+     default:
+        state = C_SMStart;
+   } // Transitions
+
+  switch(state) { // State actions
+     case C_SMStart: // Initial transition
+     	PORTD = tmpDT1 | tmpDT2 | tmpDT4; 
+        break;
+     case C_s1:
+     	PORTD = tmpDT1 | tmpDT2 | tmpDT4; 
+        break; 
+     default:
+     	PORTD = tmpDT1 | tmpDT2 | tmpDT4; 
+        break;
+  } // State actions
+  return state;
+}
+
+int TickFct_Speaker(int state) {
+	unsigned char tmpA = (~PINA & 0x04) >> 2; 
+  switch(state) { // Transitions
+     case SP_SMStart: // Initial transition
+        tmpDT4 = 0; // Initialization behavior
+        state = SP_s0;
+        break;
+     case SP_s0:
+	if(tmpA == 0x01)
+		state = SP_s1;
+	else
+		state = SP_s0;
+     case SP_s1:
+	if(tmpA == 0x01)
+        	state = SP_s1;
+	else
+		state = SP_s0;
+        break;
+     default:
+        state = SP_SMStart;
+   } // Transitions
+
+  switch(state) { // State actions
+     case SP_s0:
+        tmpDT4 = 0x00;
+        break;
+     case SP_s1:
+        tmpDT4 ^= 0x10;
+        break;
+     default:
+        break;
+  } // State actions
+  return state;
+}
+
+int TickFct_FRQ(int state) {
+unsigned char tmpA = (~PINA & 0x03);
+  switch(state) { // Transitions
+     case FRQ_SMStart: // Initial transition
+        state = FRQ_s0;
+        break;
+     case FRQ_s0:
+	if(tmpA == 0x00)
+		state = FRQ_s0;
+	else if(tmpA == 0x01)
+		state = FRQ_dec;
+	else if(tmpA == 0x02)
+		state = FRQ_inc;
+	break;
+     case FRQ_inc:
+	state = FRQ_s0;
+        break;
+     case FRQ_dec:
+	state = FRQ_s0;
+        break;
+     default:
+        state = FRQ_SMStart;
+   } // Transitions
+
+  switch(state) { // State actions
+     case FRQ_s0:
+  	//tasks[3].period = 1;
+        break;
+     case FRQ_inc:
+        periodSpeaker--;
+  	tasks[3].period = periodSpeaker;
+        break;
+     case FRQ_dec:
+        periodSpeaker++;
+  	tasks[3].period = periodSpeaker;
+        break;
+     default:
+        break;
+  } // State actions
+  return state;
+}
+
+
 int main(void)
 {
 	unsigned char x;
@@ -84,3 +489,4 @@ int main(void)
 		}
 	}
 }
+*/
